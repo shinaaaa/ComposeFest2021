590560c2e3719e9aa16fd1863383e33acaabe285
shinaaaa <shin029abc@gmail.com>
2021-12-05 23:54:36 +0900

    feat: Material Components

---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/layoutsinjetpackcompose/MaterialComponentsActivity.kt
@@ -0,0 +1,63 @@
+package com.example.layoutsinjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.layoutsinjetpackcompose.ui.theme.Layouts_in_Jetpack_ComposeTheme
+
+class MaterialComponentsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutsCodelab()
+        }
+    }
+}
+
+@Composable
+private fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
+    }
+}
+
+@Composable
+private fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview
+@Composable
+fun MaterialComponentsActivityPreview() {
+    Layouts_in_Jetpack_ComposeTheme {
+        LayoutsCodelab()
+    }
+}
